da11d8fdd2b31d20e0b3bc8ed04e591ac0a4baf1
Axel Ancona Esselmann <axel@anconaesselmann.com>
2018-01-21 00:59:44 -0800

    Bugfix

--- AAE/Classes/LocationOperators.swift
+++ AAE/Classes/LocationOperators.swift
@@ -3,6 +3,7 @@
 //
 
 import CoreLocation
+import MapKit
 
 public func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
     return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
